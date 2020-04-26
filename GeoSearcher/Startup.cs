7ab6b88b83e236fe0be3dea10d769cb2e4ee47ff
Artur Salakhov <gsmilexd@gmail.com>
2020-04-26 20:17:44 +0300

    GeoSearcher: add geobase

--- GeoSearcher/Startup.cs
+++ GeoSearcher/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeoReader.Reader;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,7 @@ namespace GeoSearcher
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGeoReader>(new GeoFileReader(Configuration["GeobasePath"]));
             services.AddRazorPages();
         }
 
