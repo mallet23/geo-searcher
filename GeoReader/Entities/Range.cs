c322bdb8e95e5139d5064cf6b5cfffacc14fe26e
Artur Salakhov <gsmilexd@gmail.com>
2020-04-26 18:17:51 +0300

    init

---
+++ GeoReader/Entities/Range.cs
@@ -0,0 +1,14 @@
+﻿﻿using System.Runtime.InteropServices;
+
+namespace GeoDatabase.Entities
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 12)]
+    public struct Range
+    {
+        public uint ip_from; // начало диапазона IP адресов
+
+        public uint ip_to; // конец диапазона IP адресов
+
+        public uint location_index; // индекс записи о местоположении
+    }
+}
